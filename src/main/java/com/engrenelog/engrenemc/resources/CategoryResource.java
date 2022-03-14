f20997739d576a6fa04628de9b7387a9e732afe3
thiagossjc <thiagossjc@gmail.com>
2022-03-14 17:52:38 +0100

    gestion de la primer class de domínio

--- src/main/java/com/engrenelog/engrenemc/resources/CategoryResource.java
+++ src/main/java/com/engrenelog/engrenemc/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.engrenelog.engrenemc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.engrenemc.domain.Category;
+
 @RestController
 @RequestMapping(value="/categorys")
+
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listing() {
-		return "Rest está funcionando!";
+	public List<Category> listing() {
+		Category cat1 = new Category(1,"Informática");
+		Category cat2 = new Category(2,"Escritório");
+				
+		//Criar un Array List
+		List<Category> lists = new ArrayList<>();		//interface
+		lists.add(cat1);
+		lists.add(cat2);
+		
+		return lists;
 	}
-
 }
