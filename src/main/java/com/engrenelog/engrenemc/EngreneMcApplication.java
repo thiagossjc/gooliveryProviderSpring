3600014d92f4cc484e6af66cea6243d6382696d5
thiagossjc <thiagossjc@gmail.com>
2022-03-14 13:40:44 +0100

    priver version proryect create

---
+++ src/main/java/com/engrenelog/engrenemc/EngreneMcApplication.java
@@ -0,0 +1,13 @@
+package com.engrenelog.engrenemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngreneMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngreneMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engrenelog/engrenemc/EngreneMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engrenelog.engrenemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngreneMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
