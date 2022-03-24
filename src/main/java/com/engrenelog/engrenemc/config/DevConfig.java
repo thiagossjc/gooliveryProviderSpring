bba16452d75b4194cfdbd9026e938d2e46392195
thiagossjc <thiagossjc@gmail.com>
2022-03-24 10:05:41 +0100

    criando banco de desarrollo

--- src/main/java/com/engrenelog/engrenemc/config/DevConfig.java
+++ src/main/java/com/engrenelog/engrenemc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.engrenelog.engrenemc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,14 +11,21 @@ import org.springframework.context.annotation.Profile;
 import com.engrenelog.engrenemc.services.DbService;
 
 @Configuration
-@Profile("dev")
-public class TestConfig {
+@Profile("test")
+public class DevConfig {
 
 	@Autowired
 	private DbService dbService;
 	
+	@Value("${spring.jpa.hibernate.dll-auto}")
+	private String dllAuto;
+
+	
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
+		if (!"create".equals(dllAuto)) {
+			return false;
+		}
 		dbService.instantiateTestDataBase();
 		return true;
 	}
