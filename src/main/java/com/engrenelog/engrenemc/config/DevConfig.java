d074d83958d5bf8a969b4f621a9ac53230ebef1a
thiagossjc <thiagossjc@gmail.com>
2022-03-24 13:10:11 +0100

    email smtp google

--- src/main/java/com/engrenelog/engrenemc/config/DevConfig.java
+++ src/main/java/com/engrenelog/engrenemc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.engrenelog.engrenemc.services.DbService;
+import com.engrenelog.engrenemc.services.EmailService;
+import com.engrenelog.engrenemc.services.SmtpEmailService;
 
 @Configuration
 @Profile("test")
@@ -28,5 +30,10 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
